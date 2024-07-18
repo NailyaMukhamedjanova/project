a449a5c38498e2bd6e2fc7de5e94ade07ce09946
Nailya M <nelechka9222@gmail.com>
2024-07-18 13:25:02 +0500

    Card/задала bootom z-index

--- frontend/partyshaker/src/Components/ProductCart/ProductCart.jsx
+++ frontend/partyshaker/src/Components/ProductCart/ProductCart.jsx
@@ -1,68 +1,74 @@
 import styles from './ProductCart.module.css';
 import React from 'react';
-import {Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css'
 import { Slider } from '../Slider/Slider';
+import { Scrollbar, A11y } from 'swiper/modules'
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import 'swiper/css/a11y';в
+import 'swiper/swiper-bundle.css';
+
 import slides from './mock.json'
-// import { Link } from "react-router-dom"; 
 
 
-function ProductCart({}) {
+function ProductCart({slide, ...props}) {
+  
+    
+    
 
     return (
-        <>
-        <div className={styles['frame']}>
-<Slider  slides={slides}/>
-{/* <Swiper
-spaceBetween={50}
-slidesPerView ={1}
-onSlideChange={()=> console.log('slide change')}    
-// onSwiper={(swiper) = console.log(swiper) }
->
-                    <SwiperSlide><img className={styles['frame-img']} src='./cartImg1.png'></img>
-                </SwiperSlide>
-                    <SwiperSlide><img className={styles['frame-img']} src='./cartImg2.png'></img>
-                </SwiperSlide>
-                    <SwiperSlide><img className={styles['frame-img']} src='./cartImg3.png'></img>
-                </SwiperSlide>
-</Swiper> */}
-
-
-
-
-
-
-
-
-                {/* <img className={styles['frame-img']}  src ='./cartImg1.png'></img> */}
-                
+     
+        
+  
+            
+            <div className={styles['frame']} >
                 <button className={styles['frame-button']}> <img src="./icons/iconBtn.svg" alt="" /></button>
+                
+                
+                {/* <Slider slides={slides} /> */}
+
+            <Swiper
+
+                modules={[Scrollbar, A11y]}
+
+                slidesPerView={1}
+                loop={true}
+
+                scrollbar={{ draggable: true }} >
+
+                {slides.map((slide) =>
+                (<SwiperSlide key={slide.image}  >
+                    <img className={styles['frame-img']} src={slide.image} alt={slide.title} />
+                </SwiperSlide>))}
+            </Swiper>
 
 
                 <div className={styles['bottom']}>
                     <div className={styles['bottom-frame']}>
-                        <h3 className={styles['bottom-title']} >Апероллини</h3>
-                    
-                        <p className={styles['bottom-text']}> Сладкий</p>
+                        <h3 className={styles['bottom-title']} > Лонг-Айленд</h3>
+                        <p className={styles['bottom-text']}> Сладкий </p>
                         <div className={styles['bottom-icons']} >
                             <div className={styles['bottom-icon']} >
-                            <img src="./icons/semiAlcogol.svg" alt="" />
-                            <img src="./icons/iconShort.svg" alt="" />
-                            <img src="./icons/mediumDift.svg" alt="" />
+                                <img src="./icons/fortress/iconNonAlc.svg" alt="" />
+                                <img src="./icons/format/iconShort.svg" alt="" />
+                                <img src="./icons/complexity/iconEasy.svg" alt="" />
                             </div>
                             <div className={styles['bottom-reiting']}>
-                            <span>4.2</span>
+                                <span>4.2</span>
                                 <img width='12px' height='12px' src="./icons/iconStar.svg" alt="" />
+                            </div>
                         </div>
-                        </div> 
                     </div>
                 </div>
+
+
+            </div>
         
-        
-        </div>
-       
-        </>
+                 
+            
+   
     )
 }
 
-export default ProductCart; 
+export default ProductCart;
--- frontend/partyshaker/src/Components/Slider/Slider.jsx
+++ frontend/partyshaker/src/Components/Slider/Slider.jsx
@@ -2,40 +2,35 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './Slider.module.css';
 import { Scrollbar, A11y } from 'swiper/modules'
 import 'swiper/css';
-
 import 'swiper/css/scrollbar';
 import 'swiper/css/a11y';
-
-
 import 'swiper/swiper-bundle.css';
+import { Children, cloneElement, useEffect, useState } from 'react';
 
+export const Slider = ({ slides }) => {
+  
 
-
-
-
-export const Slider = ({slides}) => {
     return (
-    <>     
-    <Swiper 
-    
-                modules={[ Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={1}
-
- 
-    scrollbar={{draggable:true}}
-        onSlideChange={() => console.log('slide change')}
-    // onSwiper={(swiper) = console.log(swiper) }
-    >
-
-        {slides.map((slide) => (<SwiperSlide key={slide.image} ><img className={styles['frame-img']} src={slide.image} alt = {slide.title}/>
-        </SwiperSlide>) )}
+        <>
+        <div className={styles['swiper']}> 
+            <Swiper
         
-      
-    </Swiper>
-    </>)
+                modules={[Scrollbar, A11y]}
+       
+                slidesPerView={1}
+                loop = {true}
+
+                scrollbar={{ draggable: true }} >
+
+                {slides.map((slide) =>
+                (<SwiperSlide key={slide.image}  >
+                <img className={styles['frame-img']} src={slide.image} alt={slide.title} />
+                </SwiperSlide>))}
+            </Swiper>
+            </div>
+        </>)
 
 
 
-    
+
 }
